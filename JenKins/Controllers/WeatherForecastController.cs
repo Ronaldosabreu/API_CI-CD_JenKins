b1a4cd52991c783059ca12d135fd1021223c4045
Ronaldo Soares Abreu <ronaldosoares.a@gmail.com>
2022-05-31 17:18:33 -0300

    alteração jenkins

--- JenKins/Controllers/WeatherForecastController.cs
+++ JenKins/Controllers/WeatherForecastController.cs
@@ -17,7 +17,7 @@ namespace JenKins.Controllers
 
     private static readonly string[] Summaries = new[]
     {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "BLAAAA Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
     private readonly ILogger<WeatherForecastController> _logger;
